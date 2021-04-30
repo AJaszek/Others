6d78b83e474fcf551131d9480636048c19806c51
AJaszek <31315854+AJaszek@users.noreply.github.com>
2021-04-30 09:54:28 +0200

    Add files via upload

---
+++ MaszynaHUD/setna_proba/Main.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace setna_proba
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnMissles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.warAdress = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMissles
+            // 
+            this.btnMissles.Location = new System.Drawing.Point(74, 41);
+            this.btnMissles.Name = "btnMissles";
+            this.btnMissles.Size = new System.Drawing.Size(75, 23);
+            this.btnMissles.TabIndex = 0;
+            this.btnMissles.Text = "naboje";
+            this.btnMissles.UseVisualStyleBackColor = true;
+            this.btnMissles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "igi2",
+            "maszyna"});
+            this.comboBox1.Location = new System.Drawing.Point(237, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "7ff768c50000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Adres bazy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Wybór programu";
+            // 
+            // warAdress
+            // 
+            this.warAdress.AutoSize = true;
+            this.warAdress.ForeColor = System.Drawing.Color.Red;
+            this.warAdress.Location = new System.Drawing.Point(71, 158);
+            this.warAdress.Name = "warAdress";
+            this.warAdress.Size = new System.Drawing.Size(168, 13);
+            this.warAdress.TabIndex = 7;
+            this.warAdress.Text = "Aplikacja 64-bit - Wprowadź adres";
+            this.warAdress.Visible = false;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(279, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "labelSpeed";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 201);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.warAdress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMissles);
+            this.Name = "Main";
+            this.Text = "A sprobuj nie zadzialac!";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMissles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label warAdress;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MaszynaHUD/setna_proba/Main.cs
@@ -0,0 +1,198 @@
+﻿using setna_proba.Utilities;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace setna_proba
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        uint pAddress = 0x075F9B44;
+        uint offset1 = 0xFAC;
+        uint offset2 = 0x28;
+        uint offset3 = 0x4;
+        ulong offset = 0;
+        uint offsett = 0;
+        ulong offsetSpeed = 0;
+        ulong offsetSpeedd = 0;
+        int value=0;
+        int oldValue=0;
+        int valueSpeed=0;
+        float valueSpeedd=0;
+        Process process;
+        ProccessMemoryReader mreader = new ProccessMemoryReader();
+        int bytesOut = 0;
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        private void igi()
+        {
+            
+            process = Process.GetProcessesByName("igi2").ToList().FirstOrDefault();
+            //Process process = Process.GetProcessById(10176);
+            if (process != null)
+            {
+                mreader.ReadProcess = process;
+                mreader.OpenProcess();
+
+                offset = BitConverter.ToUInt32(mreader.ReadMemory((IntPtr)(pAddress + (uint)process.Modules[0].BaseAddress), 4, out bytesOut), 0);
+
+                if (offset3 < 0)
+                    offset -= (uint)Math.Abs(offset3);
+                else
+                    offset += (uint)offset3;
+
+                offsett = BitConverter.ToUInt32(mreader.ReadMemory((IntPtr)offset, 4, out bytesOut), 0);
+
+                offsett += (uint)offset2;
+
+                offset = BitConverter.ToUInt32(mreader.ReadMemory((IntPtr)offsett, 4, out bytesOut), 0);
+
+                offset += (uint)offset1;
+
+                // offsett = BitConverter.ToUInt32(mreader.ReadMemory((IntPtr)offsett, 4, out bytesOut), 0);
+
+                value = BitConverter.ToInt32(mreader.ReadMemory((IntPtr)offset, 4, out bytesOut), 0);
+
+                process.Close();
+            }
+            
+
+        }
+         private void maszyna()
+        {
+            if (!serialPort1.IsOpen) serialPort1.Open();
+            process = Process.GetProcessesByName("eu07").ToList().FirstOrDefault();
+            if (textBox1.Text == "")
+                warAdress.Visible = true;
+            else
+            {
+                if (process != null)
+                {
+                    
+                    SetForegroundWindow(process.MainWindowHandle);
+                    warAdress.Visible = false;
+                    ulong baza = Convert.ToUInt64(textBox1.Text, 16);
+                    //baza = 0x7ff69a4a0000;
+                    //ulong baza = (ulong)numericUpDown1.Value;
+                    mreader.ReadProcess = process; 
+                    mreader.OpenProcess();
+                    //offset = (0x2EF4B8 + (ulong)process.Modules[0].BaseAddress);
+                    offset = 0x2EF4B8 + baza;
+                    //offset = BitConverter.ToUInt64(mreader.ReadMemory((IntPtr)(0x2EF4B8 + (uint)process.Modules[0].BaseAddress), 4, out bytesOut), 0);
+                    label1.Text = offset.ToString();
+
+                   // offsetSpeed = baza + 0x2F1358;
+                    offsetSpeedd = BitConverter.ToUInt64(mreader.ReadMemory((IntPtr)(baza + 0x2F1358), 8, out bytesOut), 0);
+                    offsetSpeedd += 0x3a590;
+
+                    timer1.Enabled = true;
+                }
+            }
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+           // maszyna();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // mreader.WriteMemory((IntPtr)offset, BitConverter.GetBytes(4104), out bytesOut);
+
+            /* SetForegroundWindow(process.MainWindowHandle);
+             //for(int i=0;i<100;i++)
+             SendKeys.SendWait("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+             */
+            String aaa = serialPort1.ReadLine();
+            label1.Text = aaa;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            value = BitConverter.ToInt32(mreader.ReadMemory((IntPtr)offset, 4, out bytesOut), 0);
+            // valueSpeed = BitConverter.ToFloat(mreader.ReadMemory((IntPtr)offsetSpeed, 4, out bytesOut), 0);
+            //valueSpeedd = BitConverter.ToInt32(mreader.ReadMemory((IntPtr)offsetSpeedd, 4, out bytesOut), 0);
+            valueSpeedd = BitConverter.ToSingle(mreader.ReadMemory((IntPtr)offsetSpeedd, 4, out bytesOut),0);
+
+            //byte[] bytesOfTheNumber = mreader.ReadMemory((IntPtr)offsetSpeedd, 4, out bytesOut);
+            //valueSpeedd = BitConverter.ToSingle(bytesOfTheNumber, 0);
+            //label1.Text = offset.ToString();
+             label2.Text = value.ToString();
+            //label5.Text = Convert.ToSingle(valueSpeedd).ToString();
+            label5.Text = valueSpeedd.ToString();
+            //
+            if (serialPort1.IsOpen)
+            {
+                if (valueSpeedd > 5.0)
+                {
+                    int speeed = (int)valueSpeedd;
+                    serialPort1.WriteLine(speeed.ToString()+'x');
+                    
+                    //
+                   // String aaa = serialPort1.ReadLine();
+                    
+                    //label1.Text += "p";
+                }
+                else
+                {
+                    serialPort1.Write("0");
+                }
+                /*  if (value == 4096)
+                  {
+                      serialPort1.Write("0");
+
+
+
+                      //label1.Text += serialPort1.ReadLine();
+                  }
+                  else
+                  {*/
+                if (value != oldValue)
+                {
+                    serialPort1.Write(value.ToString() + 'd');
+                    label1.Text = serialPort1.ReadExisting();
+                    oldValue = value;
+                }
+                //}
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedItem.ToString();
+            if (comboBox1.SelectedItem.ToString() == "igi2")
+                igi();
+            else
+                maszyna();
+
+
+
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
